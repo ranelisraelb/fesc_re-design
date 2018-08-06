ca9e1f793f15c6b52fc65bcacd0595807bba60e8
Ranel Bautista <ranelisraelb@gmail.com>
2018-08-05 22:33:26 -0700

    added 1960-2015 filter, still can't get 1st script when 2nd is active

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,82 @@ google.charts.load('current', {
     'packages': ['corechart']
 });
 
-google.charts.setOnLoadCallback(drawFirstStretch);
+google.charts.setOnLoadCallback(drawWholeStretch);
+
+function drawWholeStretch() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Year');
+    data.addColumn('number', 'Billion BTU');
+    data.addRows([
+        ['1960', 35680],
+        ['1961', 34417],
+        ['1962', 35826],
+        ['1963', 38613],
+        ['1964', 39016],
+        ['1965', 39916],
+        ['1966', 42760],
+        ['1967', 44550],
+        ['1968', 49556],
+        ['1969', 51489],
+        ['1970', 51035],
+        ['1971', 49968],
+        ['1972', 54389],
+        ['1973', 56250],
+        ['1974', 52429],
+        ['1975', 50007],
+        ['1976', 56507],
+        ['1977', 59953],
+        ['1978', 65357],
+        ['1978', 65357],
+        ['1979', 69419],
+        ['1980', 90049],
+        ['1981', 83117],
+        ['1982', 104674],
+        ['1983', 91705],
+        ['1984', 108740],
+        ['1985', 110698],
+        ['1986', 116356],
+        ['1987', 107606],
+        ['1988', 113802],
+        ['1989', 232261],
+        ['1990', 198986],
+        ['1991', 212955],
+        ['1992', 230779],
+        ['1993', 217028],
+        ['1994', 215563],
+        ['1995', 220211],
+        ['1996', 240343],
+        ['1996', 240343],
+        ['1997', 231308],
+        ['1998', 205485],
+        ['1999', 204114],
+        ['2000', 194952],
+        ['2001', 158038],
+        ['2002', 174327],
+        ['2003', 188473],
+        ['2004', 179462],
+        ['2005', 183175],
+        ['2006', 185564],
+        ['2007', 190489],
+        ['2008', 195232],
+        ['2009', 213642],
+        ['2010', 223518],
+        ['2011', 222956],
+        ['2012', 220020],
+        ['2013', 229666],
+        ['2014', 226863],
+        ['2015', 234192]
+    ]);
+
+    var options = {
+        'title': 'Renewable Energy Production In Florida (1960 - 2015)',
+        'width': 400,
+        'height': 300,
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('1960-1978'));
+    chart.draw(data, options);
+}
 
 function drawFirstStretch() {
     var data = new google.visualization.DataTable();
@@ -115,7 +190,15 @@ function drawThirdStretch() {
 
 window.onload = function () {
     // Event listeners for each button: clear all charts, redraw requested one
+    document.getElementById('1960-2015-trigger').addEventListener('click', function () {
+        document.getElementById('1960-2015').innerHTML = ""
+        document.getElementById('1960-1978').innerHTML = ""
+        document.getElementById('1978-1996').innerHTML = ""
+        document.getElementById('1996-2015').innerHTML = ""
+        drawWholeStretch();
+    })
     document.getElementById('1960-1978-trigger').addEventListener('click', function () {
+        document.getElementById('1960-2015').innerHTML = ""
         document.getElementById('1960-1978').innerHTML = ""
         document.getElementById('1978-1996').innerHTML = ""
         document.getElementById('1996-2015').innerHTML = ""
@@ -123,6 +206,7 @@ window.onload = function () {
     })
 
     document.getElementById('1978-1996-trigger').addEventListener('click', function () {
+        document.getElementById('1960-2015').innerHTML = ""
         document.getElementById('1960-1978').innerHTML = ""
         document.getElementById('1978-1996').innerHTML = ""
         document.getElementById('1996-2015').innerHTML = ""
@@ -130,6 +214,7 @@ window.onload = function () {
     })
 
     document.getElementById('1996-2015-trigger').addEventListener('click', function () {
+        document.getElementById('1960-2015').innerHTML = ""
         document.getElementById('1960-1978').innerHTML = ""
         document.getElementById('1978-1996').innerHTML = ""
         document.getElementById('1996-2015').innerHTML = ""
--- scripts/script2.js
+++ scripts/script2.js
@@ -2,7 +2,83 @@ google.charts.load('current', {
     'packages': ['corechart']
 });
 
-google.charts.setOnLoadCallback(drawFirstConsumption);
+google.charts.setOnLoadCallback(drawConsumption);
+
+function drawConsumption() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Year');
+    data.addColumn('number', 'Billion BTU');
+    data.addRows([
+        ['1960', 57344],
+        ['1961', 62705],
+        ['1962', 71216],
+        ['1963', 78258],
+        ['1964', 87016],
+        ['1965', 95878],
+        ['1966', 108610],
+        ['1967', 119314],
+        ['1968', 135509],
+        ['1969', 153032],
+        ['1970', 171346],
+        ['1971', 188564],
+        ['1972', 209665],
+        ['1973', 237104],
+        ['1974', 235482],
+        ['1975', 242096],
+        ['1976', 252408],
+        ['1977', 270767],
+        ['1978', 289031],
+        ['1978', 289031],
+        ['1979', 295551],
+        ['1980', 309694],
+        ['1981', 317921],
+        ['1982', 315744],
+        ['1983', 329216],
+        ['1984', 353246],
+        ['1985', 379307],
+        ['1986', 398095],
+        ['1987', 417862],
+        ['1988', 444382],
+        ['1989', 472473],
+        ['1990', 489741],
+        ['1991', 499299],
+        ['1992', 501598],
+        ['1993', 521176],
+        ['1994', 544365],
+        ['1995', 571483],
+        ['1996', 586291],
+        ['1996', 586291],
+        ['1997', 597240],
+        ['1998', 639254],
+        ['1999', 638966],
+        ['2000', 668216],
+        ['2001', 684966],
+        ['2002', 718136],
+        ['2003', 741696],
+        ['2004', 745810],
+        ['2005', 767622],
+        ['2006', 778685],
+        ['2007', 788461],
+        ['2008', 771702],
+        ['2009', 766848],
+        ['2010', 788887],
+        ['2011', 768009],
+        ['2012', 752941],
+        ['2013', 757189],
+        ['2014', 771379],
+        ['2015', 803865],
+        ['2016', 804283]
+    ]);
+
+    var options = {
+        'title': 'Renewable Energy Consumption In Florida (1960 - 2016)',
+        'width': 400,
+        'height': 300,
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('all-con'));
+    chart.draw(data, options);
+}
 
 function drawFirstConsumption() {
     var data = new google.visualization.DataTable();
@@ -116,7 +192,15 @@ function drawThirdConsumption() {
 
 window.onload = function () {
     // Event listeners for each button: clear all charts, redraw requested one
+    document.getElementById('all-trigger-con').addEventListener('click', function () {
+        document.getElementById('all-con').innerHTML = ""
+        document.getElementById('1960-1978-con').innerHTML = ""
+        document.getElementById('1978-1996-con').innerHTML = ""
+        document.getElementById('1996-2016-con').innerHTML = ""
+        drawConsumption();
+    })
     document.getElementById('1960-1978-trigger-con').addEventListener('click', function () {
+        document.getElementById('all-con').innerHTML = ""
         document.getElementById('1960-1978-con').innerHTML = ""
         document.getElementById('1978-1996-con').innerHTML = ""
         document.getElementById('1996-2016-con').innerHTML = ""
@@ -124,6 +208,7 @@ window.onload = function () {
     })
 
     document.getElementById('1978-1996-trigger-con').addEventListener('click', function () {
+        document.getElementById('all-con').innerHTML = ""
         document.getElementById('1960-1978-con').innerHTML = ""
         document.getElementById('1978-1996-con').innerHTML = ""
         document.getElementById('1996-2016-con').innerHTML = ""
@@ -131,6 +216,7 @@ window.onload = function () {
     })
 
     document.getElementById('1996-2016-trigger-con').addEventListener('click', function () {
+        document.getElementById('all-con').innerHTML = ""
         document.getElementById('1960-1978-con').innerHTML = ""
         document.getElementById('1978-1996-con').innerHTML = ""
         document.getElementById('1996-2016-con').innerHTML = ""
