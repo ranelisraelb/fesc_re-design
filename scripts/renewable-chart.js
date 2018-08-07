bf7a67d08fdc0a143869c75c39bf3a8ecd5d74d0
Ranel Bautista <ranelisraelb@gmail.com>
2018-08-07 08:29:45 -0700

    added renewable production api

--- scripts/renewable-chart.js
+++ scripts/renewable-chart.js
@@ -1,46 +1,73 @@
-//1 DOMContentLoaded
-//2 Google chart library loaded
-//3 get data from API
-//4 Data received from the API
-//5 Draw chart
-// db94cf5c0202d5c76c61e38805bb8373 -->
+google.charts.load('current', {
+    'packages': ['corechart']
+});
 
+google.charts.setOnLoadCallback(drawWholeStretch);
 
-function onDOMLoad() {
-    google.charts.load('current', {
-        'packages': ['corechart', 'bar']
-    })
-    google.charts.setOnLoadCallback(getData);
-}
-document.addEventListener("DOMContentLoaded", onDOMLoad)
-
-function getData() {
-    let request = new XMLHttpRequest()
-    let requestURL = "http://api.eia.gov/series/?api_key=db94cf5c0202d5c76c61e38805bb8373&series_id=SEDS.REPRB.FL.A"
-    request.open('GET', requestURL, true)
-    request.onload = function () {
-        if (request.status !== 200) {
-            console.log("Something went wrong: ", request)
-            return
-        }
-        let response = JSON.parse(request.response)
-        console.log(response.series[0].data)
-        drawWholeStretch(response.series[0].data);
-    }
-    request.error = function (err) {
-        console.log("error is: ", err)
-        return
-    }
-    request.send()
-}
-
-// google.charts.setOnLoadCallback(drawWholeStretch);
-
-function drawWholeStretch(getData) {
+function drawWholeStretch() {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Year');
     data.addColumn('number', 'Billion BTU');
-    data.addRows(getData);
+    data.addRows([
+        ['1960', 35680],
+        ['1961', 34417],
+        ['1962', 35826],
+        ['1963', 38613],
+        ['1964', 39016],
+        ['1965', 39916],
+        ['1966', 42760],
+        ['1967', 44550],
+        ['1968', 49556],
+        ['1969', 51489],
+        ['1970', 51035],
+        ['1971', 49968],
+        ['1972', 54389],
+        ['1973', 56250],
+        ['1974', 52429],
+        ['1975', 50007],
+        ['1976', 56507],
+        ['1977', 59953],
+        ['1978', 65357],
+        ['1978', 65357],
+        ['1979', 69419],
+        ['1980', 90049],
+        ['1981', 83117],
+        ['1982', 104674],
+        ['1983', 91705],
+        ['1984', 108740],
+        ['1985', 110698],
+        ['1986', 116356],
+        ['1987', 107606],
+        ['1988', 113802],
+        ['1989', 232261],
+        ['1990', 198986],
+        ['1991', 212955],
+        ['1992', 230779],
+        ['1993', 217028],
+        ['1994', 215563],
+        ['1995', 220211],
+        ['1996', 240343],
+        ['1996', 240343],
+        ['1997', 231308],
+        ['1998', 205485],
+        ['1999', 204114],
+        ['2000', 194952],
+        ['2001', 158038],
+        ['2002', 174327],
+        ['2003', 188473],
+        ['2004', 179462],
+        ['2005', 183175],
+        ['2006', 185564],
+        ['2007', 190489],
+        ['2008', 195232],
+        ['2009', 213642],
+        ['2010', 223518],
+        ['2011', 222956],
+        ['2012', 220020],
+        ['2013', 229666],
+        ['2014', 226863],
+        ['2015', 234192]
+    ]);
 
     var options = {
         'title': 'Renewable Energy Production In Florida (1960 - 2015)',
@@ -51,10 +78,6 @@ function drawWholeStretch(getData) {
     var chart = new google.visualization.LineChart(document.getElementById('1960-1978'));
     chart.draw(data, options);
 }
-
-
-
-
 
 function drawFirstStretch() {
     var data = new google.visualization.DataTable();
@@ -91,7 +114,6 @@ function drawFirstStretch() {
     var chart = new google.visualization.LineChart(document.getElementById('1960-1978'));
     chart.draw(data, options);
 }
-
 
 function drawSecondStretch() {
     var data = new google.visualization.DataTable();
@@ -173,7 +195,7 @@ window.onload = function () {
         document.getElementById('1960-1978').innerHTML = ""
         document.getElementById('1978-1996').innerHTML = ""
         document.getElementById('1996-2015').innerHTML = ""
-        drawWholeStretch(getData);
+        drawWholeStretch();
     })
     document.getElementById('1960-1978-trigger').addEventListener('click', function () {
         document.getElementById('1960-2015').innerHTML = ""
